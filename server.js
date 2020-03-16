5d65c33da22c516b56e663536fed91e3fffa138e
risrael-1 <rapha.israel@live.fr>
2020-03-16 15:31:40 +0100

    execrice 3

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/hello/:nom', function (req, res) {
+    if(res.param.nom)
+    {
+        res.send('Bonjour !', req.param.nom)
+    } else {
+        res.send('Quel est votre nom ?')
+    }
+  })
+
 app.listen(PORT, function () {
   console.log('Example app listening on port ${PORT} !')
 })
