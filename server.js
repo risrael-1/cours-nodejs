9249e9d1c5d21100d04bc85d57cb9930a7bc4f47
risrael-1 <rapha.israel@live.fr>
2020-03-17 10:46:26 +0100

    add post/chat endpoint

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+
+app.use(express.json()) // for parsing application/json
+
 const PORT = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
@@ -14,6 +17,16 @@ app.get('/hello', function(req, res){
 	}
 });
 
+app.post('/chat', function (req, res) {
+	if (req.body.msg === 'ville') {
+	  res.send('Nous sommes à Paris')
+	} else if (req.body.msg === 'météo') {
+	  res.send('Il fait beau')
+	} else {
+	  res.send(req.body.msg)
+	}
+  })
+
 app.listen(PORT, function () {
-  console.log('Example app listening on port ${PORT} !')
+  console.log('Example app listening on port ' + PORT)
 })
