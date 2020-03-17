583b0c9ee765b6802a01482d1bbc65fbcd6d5edb
risrael-1 <rapha.israel@live.fr>
2020-03-17 14:29:41 +0100

    ex 1 part 5

--- server.js
+++ server.js
@@ -1,32 +1,52 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
 
 app.use(express.json()) // for parsing application/json
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.get('/hello', function(req, res){
-	if(req.query.nom){
-		res.send("Bonjour " + req.query.nom + "!\n");
-	}else{
-		res.send("Quel es ton nom ?\n");
-	}
-});
+app.get('/hello', function (req, res) {
+  const nom = req.query.nom
+  if (nom) {
+    res.send('Bonjour, ' + nom + ' !')
+  } else {
+    res.send('Quel est votre nom ?')
+  }
+})
 
 app.post('/chat', function (req, res) {
-	if (req.body.msg === 'ville') {
-	  res.send('Nous sommes à Paris')
-	} else if (req.body.msg === 'météo') {
-	  res.send('Il fait beau')
-	} else {
-	  res.send(req.body.msg)
-	}
-  })
+  if (req.body.msg === 'ville') {
+    res.send('Nous sommes à Paris')
+  } else if (req.body.msg === 'météo') {
+    res.send('Il fait beau')
+  } else {
+    if (/ = /.test(req.body.msg)) {
+      const [ cle, valeur ] = req.body.msg.split(' = ')
+      const valeursExistantes = readValuesFromFile();
+      fs.writeFileSync('réponses.json', JSON.stringify({
+        ...valeursExistantes,
+        [cle]: valeur
+      }))
+      res.send('Merci pour cette information !')
+    } else {
+      const cle = req.body.msg
+      const reponse = readValuesFromFile()[cle]
+      res.send(cle + ': ' + reponse)
+    }
+  }
+})
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT)
 })
+
+function readValuesFromFile() {
+  const reponses = fs.readFileSync('réponses.json', { encoding: 'utf8' });
+  const valeursExistantes = JSON.parse(reponses);
+  return valeursExistantes;
+}
